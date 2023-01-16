823bc0194cabcb9a3d9c835ace3305e28cc61720
4rturo4lvarez <t1023600321@unitru.edu.pe>
2023-01-15 22:49:56 -0500

    database

--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ export const config =
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
 }
