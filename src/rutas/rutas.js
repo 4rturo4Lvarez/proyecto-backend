cf5165e58c80a30b547aaa2b628507766f1d57f0
4rturo4lvarez <t1023600321@unitru.edu.pe>
2023-01-16 03:22:10 -0500

    solicitud permisos

--- src/rutas/rutas.js
+++ src/rutas/rutas.js
@@ -3,12 +3,7 @@ import {
     getUser,
     getUsers,
     updatePassUser,
-    getPlaces,
-    savePlace,
-    getPermissions,
-    savePermission,
     getPermission,
-    getPlace,
     getPermissionCount,
 } from '../solicitudes/solicitudes.js';
 
@@ -19,16 +14,6 @@ router.get('/users', getUsers)
 router.get('/users/:dni', getUser)
 
 router.put('/users/:dni', updatePassUser)
-
-router.get('/places', getPlaces)
-
-router.get('/places/:codigo', getPlace)
-
-router.post('/places', savePlace)
-
-router.get('/permissions', getPermissions)
-
-router.post('/permissions', savePermission)
 
 router.get('/permissions/:dni', getPermission)
 
--- src/solicitudes/solicitudes.js
+++ src/solicitudes/solicitudes.js
@@ -36,74 +36,11 @@ export const updatePassUser = async (req, res) => {
 
 //=========================================================
 
-// Obtener el listado de todos los espacios
-export const getPlaces = async (req, res) => {
-    const connection = await connect();
-    const [rows] = await connection
-        .query('SELECT * FROM Espacios')
-
-    res.json(rows);
-};
-
-//Obtener un solo espacio por su codigo
-export const getPlace = async (req, res) => {
-    const connection = await connect();
-    const [rows] = await connection.
-        query('SELECT * FROM Espacios WHERE CODIGO = ?',
-            [req.params.codigo,]
-        );
-
-    res.json(rows[0]);
-};
-
-// Crear un nuevo espacio
-export const savePlace = async (req, res) => {
-    const connection = await connect();
-    const [results] = await connection
-        .query('INSERT INTO Espacios(CODIGO, Nombre) VALUES (?, ?)',
-            [
-                req.body.codigo,
-                req.body.nombre
-            ]
-        );
-
-    res.json({
-        ...req.body
-    });
-};
-
-//=========================================================
-
-// Obtener el listado de todas las relaciones
-export const getPermissions = async (req, res) => {
-    const connection = await connect();
-    const [rows] = await connection
-        .query('SELECT * FROM Espacios_has_Usuarios');
-
-    res.json(rows);
-};
-
-// Crear relacion espacio-usuario
-export const savePermission = async (req, res) => {
-    const connection = await connect();
-    const [results] = await connection
-        .query('INSERT INTO Espacios_has_Usuarios(Espacios_CODIGO, Usuarios_DNI) VALUES (?, ?)',
-            [
-                req.body.espacio,
-                req.body.usuario
-            ]
-        );
-
-    res.json({
-        ...req.body
-    });
-};
-
 //Obtener un solo usuario por su dni
 export const getPermission = async (req, res) => {
     const connection = await connect();
     const [rows] = await connection.
-        query('SELECT Espacios_CODIGO FROM Espacios_has_Usuarios WHERE Usuarios_DNI = ?',
+        query('SELECT Espacios_CODIGO FROM Espacios_has_Usuarios WHERE EXISTS (SELECT Espacios_CODIGO FROM Espacios_has_Usuarios WHERE Usuarios_DNI = ?)',
             [req.params.dni,]
         );
 
